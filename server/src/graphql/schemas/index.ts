cc232a7e249df1620567850dc1ce0c1e33e2cfbb
pineapple45 <gomraanmol@gmail.com>
2021-07-04 23:07:21 +0530

    added full custom directives functionality

--- server/src/graphql/schemas/index.ts
+++ server/src/graphql/schemas/index.ts
@@ -1,6 +1,12 @@
 import { buildSchema } from 'graphql';
+import { addDirectiveResolveFunctionsToSchema } from 'graphql-directive';
+import { upperCase, isAuthenticated, hasAuthorisation } from '../directives';
 
 const schema = buildSchema(`
+
+directive @upperCase on FIELD_DEFINITION | FIELD
+directive @isAuthenticated on FIELD_DEFINITION | FIELD
+directive @hasAuthorisation(roles: [Role!]) on FIELD_DEFINITION | FIELD
 
 input PostInput {
     image: String
@@ -57,7 +63,7 @@ input UserInput {
 
 type User {
     _id: ID!
-    username: String! 
+    username: String! @upperCase
     email: String!
     password: String
     role: Role!
@@ -73,7 +79,7 @@ type AuthData {
 }
 
 type RootQuery {
-  listUsers: [User!] 
+  listUsers: [User!] @hasAuthorisation(roles: [ADMIN])
   getUserById(_id: ID!): User!
   listPosts: [Post!]
   getPostById(_id: ID!): Post!
@@ -86,14 +92,14 @@ type RootQuery {
 
 type RootMutation {
   createUser(user: UserInput): User
-  deleteUser(_id: ID!): String 
-  createPost(post: PostInput): Post 
-  deletePost(_id: ID!): String 
+  deleteUser(_id: ID!): String @hasAuthorisation(roles: [ADMIN])
+  createPost(post: PostInput): Post @hasAuthorisation(roles: [ADMIN])
+  deletePost(_id: ID!): String @hasAuthorisation(roles: [ADMIN,MODERATOR])
   createComment(comment: CommentInput): Comment
-  deleteComment(_id: ID!): String 
+  deleteComment(_id: ID!): String @hasAuthorisation(roles: [ADMIN,MODERATOR])
   createLike(like: LikeInput): Like
   deleteLike(_id: ID!): String
-  assignRole(role: String! , assignedBy: ID!, assignedUser: ID!): User 
+  assignRole(role: String! , assignedBy: ID!, assignedUser: ID!): User @hasAuthorisation(roles: [ADMIN])
 }
 
 schema {
@@ -101,5 +107,11 @@ schema {
   mutation: RootMutation
 }
 `);
+
+addDirectiveResolveFunctionsToSchema(schema, {
+  upperCase,
+  isAuthenticated,
+  hasAuthorisation,
+});
 
 export default schema;
