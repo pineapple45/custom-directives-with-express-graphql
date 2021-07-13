3a91e61c95ed8f7e5cad880ec27fbd53dd9670a8
pineapple45 <gomraanmol@gmail.com>
2021-07-13 17:55:35 +0530

    minor fixes

--- server/src/graphql/resolvers/utils.ts
+++ server/src/graphql/resolvers/utils.ts
@@ -9,7 +9,6 @@ import { PostType } from './posts';
 
 export const transformLike = (like: LikeType) => {
   return {
-    ...like,
     _id: like._id,
     post: singlePost.bind(this, like.postId),
     creator: singleUser.bind(this, like.creatorId),
@@ -18,8 +17,8 @@ export const transformLike = (like: LikeType) => {
 
 export const transformComment = (comment: CommentType) => {
   return {
-    ...comment,
     _id: comment._id,
+    text: comment.text,
     post: singlePost.bind(this, comment.postId),
     creator: singleUser.bind(this, comment.creatorId),
   };
@@ -27,11 +26,13 @@ export const transformComment = (comment: CommentType) => {
 
 export const transformPost = (post: PostType) => {
   return {
-    ...post,
     _id: post._id,
+    image: post.image,
+    title: post.title,
+    description: post.description,
     creator: singleUser.bind(this, post.creatorId),
-    commentList: commentsByPostId.bind(this, post._id!),
-    likeList: likesByPostId.bind(this, post._id!),
+    commentList: [], // return empty array for now. we will add comments functionality in a bit
+    likeList: [], // return empty array for now. we will add likes functionality in a bit
   };
 };
 
@@ -41,22 +42,25 @@ export const transformUser = (user: UserType) => {
       _id: 'deleted_user',
       email: 'deleted_user',
       username: 'deleted_user',
+      role: 'deleted_user',
       password: null,
-      postList: [],
-      commentList: [],
-      likeList: [],
+      postList: [], // return empty array as user has been deleted!
+      commentList: [], // return empty array as user has been deleted!
+      likeList: [], // return empty array as user has been deleted!
+      /* functionality can be increased so as all user's comments, likes or posts(for admins)
+       could be deleted before the user itself is deleted from database  */
     };
-
   return {
-    ...user,
     _id: user._id,
+    username: user.username,
+    email: user.email,
+    role: user.role,
     password: null,
-    postList: postsByCreatorId.bind(this, user._id!),
-    commentList: commentsByCreatorId.bind(this, user._id!),
-    likeList: likesByCreatorId.bind(this, user._id!),
+    postList: [], // return empty array for now. we will add posts functionality in a bit
+    commentList: [], // return empty array for now. we will add comments functionality in a bit
+    likeList: [], // return empty array for now. we will add likes functionality in a bit
   };
 };
-
 export const singleUser = async (userId: string) => {
   try {
     const user = await User.findById(userId);
