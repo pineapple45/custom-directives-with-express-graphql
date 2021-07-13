5f3e5751d8950273fef7e443cb1187cdaf6f2216
pineapple45 <gomraanmol@gmail.com>
2021-07-13 15:36:17 +0530

    utils file minor fixes

--- server/src/graphql/resolvers/utils.ts
+++ server/src/graphql/resolvers/utils.ts
@@ -6,6 +6,7 @@ export const transformUser = (user: UserType) => {
       _id: 'deleted_user',
       email: 'deleted_user',
       username: 'deleted_user',
+      role: 'deleted_user',
       password: null,
       postList: [], // return empty array as user has been deleted!
       commentList: [], // return empty array as user has been deleted!
@@ -13,10 +14,11 @@ export const transformUser = (user: UserType) => {
       /* functionality can be increased so as all user's comments, likes or posts(for admins)
        could be deleted before the user itself is deleted from database  */
     };
-
   return {
-    ...user,
     _id: user._id,
+    username: user.username,
+    email: user.email,
+    role: user.role,
     password: null,
     postList: [], // return empty array for now. we will add posts functionality in a bit
     commentList: [], // return empty array for now. we will add comments functionality in a bit
