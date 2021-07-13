ae8f431cf9563ba765792c3ba32e5060953316d2
pineapple45 <gomraanmol@gmail.com>
2021-07-13 16:34:37 +0530

    minor fixes

--- server/src/graphql/resolvers/utils.ts
+++ server/src/graphql/resolvers/utils.ts
@@ -9,6 +9,7 @@ export const transformUser = (user: UserType) => {
       _id: 'deleted_user',
       email: 'deleted_user',
       username: 'deleted_user',
+      role: 'deleted_user',
       password: null,
       postList: [], // return empty array as user has been deleted!
       commentList: [], // return empty array as user has been deleted!
@@ -16,12 +17,13 @@ export const transformUser = (user: UserType) => {
       /* functionality can be increased so as all user's comments, likes or posts(for admins)
        could be deleted before the user itself is deleted from database  */
     };
-
   return {
-    ...user,
     _id: user._id,
+    username: user.username,
+    email: user.email,
+    role: user.role,
     password: null,
-    postList: postsByCreatorId.bind(this, user._id!),
+    postList: [], // return empty array for now. we will add posts functionality in a bit
     commentList: [], // return empty array for now. we will add comments functionality in a bit
     likeList: [], // return empty array for now. we will add likes functionality in a bit
   };
@@ -29,8 +31,10 @@ export const transformUser = (user: UserType) => {
 
 export const transformPost = (post: PostType) => {
   return {
-    ...post,
     _id: post._id,
+    image: post.image,
+    title: post.title,
+    description: post.description,
     creator: singleUser.bind(this, post.creatorId),
     commentList: [], // return empty array for now. we will add comments functionality in a bit
     likeList: [], // return empty array for now. we will add likes functionality in a bit
