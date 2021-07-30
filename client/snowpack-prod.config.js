6e060874635ef69e067cd3d62a5882bcff8eb5c5
pineapple45 <gomraanmol@gmail.com>
2021-07-30 23:17:06 +0530

    client workflow added

--- client/snowpack-prod.config.js
+++ client/snowpack-prod.config.js
@@ -3,7 +3,7 @@ const path = require('path');
 /** @type {import("snowpack").SnowpackUserConfig } */
 module.exports = {
   mount: {
-    public: { url: '/custom-directives-with-express-graphql', static: true },
+    public: { url: '/', static: true },
     src: { url: '/dist' },
   },
   plugins: [
